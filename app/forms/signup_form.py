020df7d40f5ce2bf3348f647030a0031ee6364a6
Lucas <lucascostamonteiro@yahoo.com>
2022-03-15 11:05:42 -0700

    first commit

--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -1,6 +1,7 @@
+from email import message
 from flask_wtf import FlaskForm
 from wtforms import StringField
-from wtforms.validators import DataRequired, Email, ValidationError
+from wtforms.validators import DataRequired, Email, ValidationError, EqualTo
 from app.models import User
 
 
@@ -23,5 +24,6 @@ def username_exists(form, field):
 class SignUpForm(FlaskForm):
     username = StringField(
         'username', validators=[DataRequired(), username_exists])
-    email = StringField('email', validators=[DataRequired(), user_exists])
-    password = StringField('password', validators=[DataRequired()])
+    email = StringField('email', validators=[DataRequired(), Email("Must be valid email."), user_exists])
+    password = StringField('password', validators=[DataRequired(), EqualTo('repeat_password', message="Passwords must match.")])
+    repeat_password = StringField('password', validators=[DataRequired()])
