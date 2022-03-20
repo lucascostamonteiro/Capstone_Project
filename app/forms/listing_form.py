9b45c166f5f1c6b0341631b955283fe15d73218d
Lucas <lucascostamonteiro@yahoo.com>
2022-03-19 21:20:56 -0700

    forms updates

--- app/forms/listing_form.py
+++ app/forms/listing_form.py
@@ -12,6 +12,6 @@ class ListingForm(FlaskForm):
     bedroom = IntegerField('bedroom', validators=[DataRequired('Please provide number of bedroom.'), NumberRange(min=1, max=10, message='Number of bedrooms must be between 1 - 10')])
     bathroom = IntegerField('bathroom', validators=[DataRequired('Please provide number of bathroom.'), NumberRange(min=1, max=10, message='Number of bathrooms must be between 1 - 10')])
     address = StringField('address', validators=[DataRequired('Please provide an address.'), Length(min=1, max=25, message='Address must be between 5-25 characters')])
-    city = StringField('city', validators=[DataRequired('Please provide a city.'), Length(min=1, max=25, message='City must be between 1-25 characters')])
+    city = StringField('city', validators=[DataRequired('Please provide a city.'), Length(min=3, max=25, message='City must be between 1-25 characters')])
     state = StringField('state', validators=[DataRequired('Please provide a state.'), Length(min=2, max=25, message='State must be between 2-25 characters')])
     url = StringField('url', validators=[DataRequired('Please provide a valid image url.'), URL(require_tld=True, message='Please provide a valid image url.')])
