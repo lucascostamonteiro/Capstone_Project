f3b6585b65e3e728787be351b002159a0ed6e316
Lucas <lucascostamonteiro@yahoo.com>
2022-04-14 12:03:56 -0700

    calendar booking

--- react-app/src/components/BookingForm/index.js
+++ react-app/src/components/BookingForm/index.js
@@ -1,54 +1,95 @@
 import React, { useState } from "react";
+import 'react-dates/initialize';
+import { DateRangePicker } from 'react-dates';
+import 'react-dates/lib/css/_datepicker.css';
+import * as moment from 'moment';
+import './BookingForm.css'
+import './react_dates_overrides.css'
 
 const BookingForm = () => {
 
-  const now = new Date(Date.now());
-  const tomorrow = new Date(now.getDate() + 1);
-  const dayAfterTomorrow = new Date(tomorrow.getDate() + 2);
 
-  console.log('NOW', now)
-  console.log('TMRW', tomorrow);
-  console.log('DAF', dayAfterTomorrow);
+  // const now = moment();
+  const tomorrow = moment().add(1, 'days');
+  const dayAfterTomorrow = moment().add(2, 'days');
 
-  const [checkIn, setCheckIn] = useState();
-  const [checkOut, setCheckOut] = useState();
+  const [startDate, setStartDate] = useState(tomorrow);
+  const [endDate, setEndDate] = useState(dayAfterTomorrow);
   const [guests, setGuests] = useState(1);
   const [errors, setErrors] = useState([]);
-  const [render, setRender] = useState(false);
+  const [focusedInput, setFocusedInput] = useState(null);
+
+  const dateRange = ({ startDate, endDate }) => {
+    setStartDate(startDate);
+    setEndDate(endDate);
+  }
+
+  const datesOnFocusHandler = focusedInput => {
+    setFocusedInput(focusedInput)
+  }
 
   return (
-    <form>
-      <label className='booking-form'>
-        Check-in
-        <input
-          className='booking-form'
-          type='datetime-local'
-          onChange={(e) => setCheckIn(e.target.value)}
-          value={checkIn}
-        />
-      </label>
-      <label className='booking-form'>
-        Check-out
-        <input
-          className='booking-form'
-          type='datetime-local'
-          onChange={(e) => setCheckOut(e.target.value)}
-          value={checkOut}
-        />
-      </label>
-      <label className='booking-form'>
+    <form className="booking-form">
+      <DateRangePicker
+        startDate={startDate} // momentPropTypes.momentObj or null,
+        startDateId="startDate" // PropTypes.string.isRequired,
+        endDate={endDate} // momentPropTypes.momentObj or null,
+        endDateId="endDate" // PropTypes.string.isRequired,
+        // onDatesChange={({ startDate, endDate }) => setDatesSelected({ startDate, endDate })} // PropTypes.func.isRequired,
+        onDatesChange={dateRange} // PropTypes.func.isRequired,
+        focusedInput={focusedInput} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
+        onFocusChange={datesOnFocusHandler} // PropTypes.func.isRequired,
+      />
+      <label className='guest-form'>
         Guests
         <input
-          className='booking-form'
+          className='guest-input'
           type='number'
           onChange={(e) => setGuests(e.target.value)}
           value={guests}
         />
       </label>
-      <button id='booking-bar-button' type='submit'>
-        <i className="fas fa-search" />
+      <button className='booking-button' type='submit'>
+        Submit Booking
       </button>
-    </form >
+    </form>
+
+
+
+
+    // Form
+    // <form className='booking-form'>
+    //   <label >
+    //     Check-in
+    //     <input
+    //       className='check-in-form'
+    //       type='datetime-local'
+    //       onChange={(e) => setCheckIn(e.target.value)}
+    //       value={checkIn}
+    //     />
+    //   </label>
+    //   <label className='check-out-form'>
+    //     Check-out
+    //     <input
+    //       className='booking-form'
+    //       type='datetime-local'
+    //       onChange={(e) => setCheckOut(e.target.value)}
+    //       value={checkOut}
+    //     />
+    //   </label>
+    //   <label className='guest-form'>
+    //     Guests
+    //     <input
+    //       className='booking-form'
+    //       type='number'
+    //       onChange={(e) => setGuests(e.target.value)}
+    //       value={guests}
+    //     />
+    //   </label>
+    //   <button id='booking-button' type='submit'>
+    //     Book
+    //   </button>
+    // </form >
   );
 }
 
