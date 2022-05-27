fde4c7334e66793bb263b5cafc9e286cb7aa7460
Lucas <lucascostamonteiro@yahoo.com>
2022-05-27 13:52:50 -0700

    data

--- app/seeds/listings.py
+++ app/seeds/listings.py
@@ -3,7 +3,7 @@ from app.models import db, Listing
 
 def seed_listings():
   listing_1 = Listing(
-      user_id=1,
+      user_id=2,
       title="Loft by the Sea/sea view",
       description= "Studio decorated signed by interior design, all in the industrial loft style where the balcony was used to make room for the table with a bed overlooking the sea. Integrated space designed for the well-being and moments of relaxation of the tenant. Ideal for a couple or who has another child . We have a sofa bed . The apartment is fully equipped and the building offers a super infrastructure with a swimming pool ",
       price= 49,
@@ -16,7 +16,7 @@ def seed_listings():
       url="https://a0.muscache.com/im/pictures/265abca4-c237-4510-853b-1e4d6ea8d747.jpg?im_w=1200"
   )
   listing_2 = Listing(
-      user_id=1,
+      user_id=2,
       title="Casa das Apsaras - Spacious and Near the Square",
       description= "The house is located within a condominium (Altos de Trancoso), close to the beach and the Square, as is known the historic square of Trancoso (both are less than a 15-minute walk away). The condo has 24-hour security.",
       price= 495,
@@ -29,7 +29,7 @@ def seed_listings():
       url="https://a0.muscache.com/im/pictures/b6b67d91-663b-4a69-9a7a-836d0313ac41.jpg?im_w=960"
     )
   listing_3 = Listing(
-      user_id=1,
+      user_id=2,
       title="Charming house between river and sea",
       description= "Caraiva provides an experience of peace and quiet. The house has a privileged location, close to the river and Caraiva beach. The balcony is the luxury of the house, it has a beautiful look and on full moon nights, the show is on nature. Waking up to the singing of birds and sleeping with the sound of the sea makes your days in paradise even happier.",
       price= 51,
@@ -42,7 +42,7 @@ def seed_listings():
       url="https://a0.muscache.com/im/pictures/4d653506-8112-4193-9dd4-69e1a108c74a.jpg?im_w=1200"
     )
   listing_4 = Listing(
-      user_id=1,
+      user_id=2,
       title="Casa Cosenza - Milagres",
       description= """House in a closed condominium with 3 bedrooms, two suites on the upper floor and a bedroom and a social bathroom on the ground floor, with living room, kitchen and balcony in an open and interconnected environment.
       In the common area of the condominium we have a swimming pool with gazebos, barbecue, hammocks and gym.
